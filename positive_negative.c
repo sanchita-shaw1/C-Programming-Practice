0ae266ac1d9bfc1bdda04a587b8b2370efc7a4cf
sanchita-shaw1 <shawsanchita25@gmail.com>
2026-01-03 00:13:21 +0530

    positive_negative

---
+++ positive_negative.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int a;
+	printf("enter your number");
+	scanf("%d",&a);
+	if(a>0){
+	
+	printf("the number is positive",a);
+	}else{
+	printf("the number is negative",a);}
+	return 0;
+	}
